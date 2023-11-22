92bb2ea387626ca9b6df1ed479642f23c4c203a7
Nived <67339427+Nived151@users.noreply.github.com>
2023-11-21 22:32:18 -0600

    updated with 91% accuracy

--- temp1.py
+++ temp1.py
@@ -1,7 +1,27 @@
-import tensorflow as tf
-from tensorflow import keras
-import matplotlib.pyplot as plt
-#%matplotlib inline
 import numpy as np
+import matplotlib.pyplot as plt
 
-(x_train, y_train) , (x_test, y_test) = keras.datasets.mnist.load_data()
+import tensorflow as tf
+from tensorflow.keras import layers
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+
+#plt.imshow(x_train[0], cmap='Greys')
+
+# Flatten the input data
+x_train_flat = x_train.reshape((x_train.shape[0], -1))
+x_test_flat = x_test.reshape((x_test.shape[0], -1))
+
+model_lr = tf.keras.models.Sequential([
+    layers.Input(shape=(28, 28)),
+    layers.Flatten(),
+    layers.Dense(10, activation='softmax')
+])
+
+model_lr.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+y_onehot_train = tf.one_hot(y_train, 10)
+model_lr.fit(x_train, y_onehot_train, epochs=2)
+model_lr.summary()
