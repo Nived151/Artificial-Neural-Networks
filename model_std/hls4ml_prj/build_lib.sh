fa86cb0692f77042c3debc615c4c5271b9a37199
Nived <67339427+Nived151@users.noreply.github.com>
2023-11-28 12:48:28 -0600

    ubuntu run

--- model_std/hls4ml_prj/build_lib.sh
+++ model_std/hls4ml_prj/build_lib.sh
@@ -9,7 +9,7 @@ fi
 LDFLAGS=
 INCFLAGS="-Ifirmware/ap_types/"
 PROJECT=myproject
-LIB_STAMP=BBE1566f
+LIB_STAMP=868Cba60
 
 ${CC} ${CFLAGS} ${INCFLAGS} -c firmware/${PROJECT}.cpp -o ${PROJECT}.o
 ${CC} ${CFLAGS} ${INCFLAGS} -c ${PROJECT}_bridge.cpp -o ${PROJECT}_bridge.o
