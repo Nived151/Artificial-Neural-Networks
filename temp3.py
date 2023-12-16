00800c393e4a0923d33063d29079bc7dc2f2b022
Nived <67339427+Nived151@users.noreply.github.com>
2023-12-16 00:13:21 -0600

    update

--- temp3.py
+++ temp3.py
@@ -3,16 +3,13 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten
 from tensorflow.keras.utils import to_categorical
 
-# Load and preprocess the MNIST dataset
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train = x_train / 255.0
 x_test = x_test / 255.0
 
-# Convert labels to one-hot encoded format
 y_train_one_hot = to_categorical(y_train, num_classes=10)
 y_test_one_hot = to_categorical(y_test, num_classes=10)
 
-# Define the ANN model architecture
 model = Sequential([
     Flatten(input_shape=(28, 28)),
     #Dense(128, activation='relu'),  # First hidden layer with ReLU activation
@@ -20,13 +17,10 @@ model = Sequential([
     Dense(10, activation='sigmoid')  # Output layer with linear activation
 ])
 
-# Compile the model
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-# Train the model
 model.fit(x_train, y_train_one_hot, epochs=3, validation_data=(x_test, y_test_one_hot))
 
-# Summary of the model
 model.summary()
 
 test_loss, test_accuracy = model.evaluate(x_test, y_test_one_hot, verbose=0)
@@ -34,3 +28,15 @@ print(f"Test accuracy: {test_accuracy * 100:.2f}%")
 
 model.save('temp3.h5')
 
+layer_weights = model.get_weights()[0]
+layer_biases = model.get_weights()[1]
+
+# Save layer weights
+with open("layer_weight.h", 'w') as f:
+    weights_str = ', '.join(map(str, layer_weights.flatten()))
+    f.write("float layer_weight[" + str(784 * 10) + "]={" + weights_str + "};\n")
+
+# Save layer biases
+with open("layer_bias.h", 'w') as f:
+    biases_str = ', '.join(map(str, layer_biases.flatten()))
+    f.write("float layer_bias[10]={" + biases_str + "};\n")
